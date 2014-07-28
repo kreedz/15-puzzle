edda6f3769d66ce953b2309e5abb8ded944a06a4
kreedz <edgebug@gmail.com>
2014-07-28 20:03:23 +0600

    added detecting the winning

--- js.js
+++ js.js
@@ -1,8 +1,19 @@
 (function(){
+    // detecting the winning
+    function isWin(){
+        if (!tds[numbersLength - 1].innerHTML) {
+            for(var i = 1; i < numbersLength; ++i) {
+                if (tds[i - 1].innerHTML != i) return false;
+            }
+        } else return false;
+        return true;
+    }
+
     var tds = document.getElementsByTagName('td'),
         table = document.getElementsByTagName('tbody')[0],
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9],
         numbersLength = numbers.length,
+        dimension = Math.sqrt(numbersLength),
         logicFc = function(){
             var rowIndex = this.parentElement.rowIndex,
                 cellIndex = this.cellIndex,
@@ -11,18 +22,22 @@
                     a.innerHTML = b.innerHTML;
                     b.innerHTML = t;
                 }
-            // не первый столбец
+            // if not first column
             if (cellIndex && !this.previousElementSibling.innerHTML) {
                 swap(this.previousElementSibling, this);
-            // не последний столбец
-            } else if (cellIndex != Math.sqrt(numbersLength) - 1 && !this.nextElementSibling.innerHTML) {
+            // if not last column
+            } else if (cellIndex != dimension - 1 && !this.nextElementSibling.innerHTML) {
                 swap(this.nextElementSibling, this);
-            // не первая строка
-            } else if (rowIndex && !table.children[rowIndex - 1].children[cellIndex].innerHTML) {                   
+            // if not first row
+            } else if (rowIndex && !table.children[rowIndex - 1].children[cellIndex].innerHTML) {
                 swap(table.children[rowIndex - 1].children[cellIndex], this);
-            // не последняя строка
-            } else if (rowIndex != Math.sqrt(numbersLength) - 1 && !table.children[rowIndex + 1].children[cellIndex].innerHTML) {            
+            // if not last row
+            } else if (rowIndex != dimension - 1 && !table.children[rowIndex + 1].children[cellIndex].innerHTML) {
                 swap(table.children[rowIndex + 1].children[cellIndex], this);
+            }
+            
+            if (isWin()) {
+                alert("You're win!");
             }
         }
         
@@ -30,6 +45,6 @@
         var value = numbers[Math.floor(Math.random(numbers.length) * (numbersLength - i))];
         tds[i].innerHTML = numbersLength == value ? '' : value;
         numbers.splice(numbers.indexOf(value), 1);
-        tds[i].addEventListener("click", logicFc, false);        
+        tds[i].addEventListener("click", logicFc, false);
     }
 })()
