424b5189fb3118623a57987dc183aed347222cc7
kreedz <edgebug@gmail.com>
2014-07-29 23:25:53 +0600

    added several dimensions

--- js.js
+++ js.js
@@ -2,19 +2,50 @@
     // detecting the winning
     function isWin(){
         if (!tds[numbersLength - 1].innerHTML) {
-            for(var i = 1; i < numbersLength; ++i) {
+            for (var i = 1; i < numbersLength; ++i) {
                 if (tds[i - 1].innerHTML != i) return false;
             }
         } else return false;
         return true;
     }
+    
+    // create rows & cells
+    function createFields() {
+        updateValues();
+        for (var i = 0; i < dimension; ++i) {
+            var tr = tbl.insertRow(-1);
+            for (var j = 0; j < dimension; ++j) {
+                tr.insertCell(j);
+            }
+        }
+    }
+    
+    // remove all rows from table
+    function removeFields() {
+        while (tbl.firstChild) {
+            tbl.removeChild(tbl.firstChild);
+        }
+    }
+    
+    function updateValues() {
+        dimension = +document.getElementById('dimension').value;
+        numbers = [];
+        for (var i = 1; i <= dimension * dimension; ++i) {
+            numbers.push(i);
+        }
+        numbersConst = numbers.slice(0);
+        numbersLength = numbers.length;
+    }
+    
+    var numbers = [],
+        dimension = +document.getElementById('dimension').value,
+        tbl = document.getElementsByTagName('tbody')[0],
+        numbersConst = numbers.slice(0),
+        numbersLength = numbers.length;
+    
+    createFields();
 
     var tds = document.getElementsByTagName('td'),
-        table = document.getElementsByTagName('tbody')[0],
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        numbersConst = numbers.slice(0),
-        numbersLength = numbers.length,
-        dimension = Math.sqrt(numbersLength),
         logicFc = function(){
             var rowIndex = this.parentElement.rowIndex,
                 cellIndex = this.cellIndex,
@@ -30,29 +61,35 @@
             } else if (cellIndex != dimension - 1 && !this.nextElementSibling.innerHTML) {
                 swap(this.nextElementSibling, this);
             // if not first row
-            } else if (rowIndex && !table.children[rowIndex - 1].children[cellIndex].innerHTML) {
-                swap(table.children[rowIndex - 1].children[cellIndex], this);
+            } else if (rowIndex && !tbl.children[rowIndex - 1].children[cellIndex].innerHTML) {
+                swap(tbl.children[rowIndex - 1].children[cellIndex], this);
             // if not last row
-            } else if (rowIndex != dimension - 1 && !table.children[rowIndex + 1].children[cellIndex].innerHTML) {
-                swap(table.children[rowIndex + 1].children[cellIndex], this);
+            } else if (rowIndex != dimension - 1 && !tbl.children[rowIndex + 1].children[cellIndex].innerHTML) {
+                swap(tbl.children[rowIndex + 1].children[cellIndex], this);
             }
             
             if (isWin()) {
                 alert("You're win!");
             }
         }
-        
-    var fillFields = function(){
+    
+    // fill fields with numbers
+    var fillFields = function() {
         numbers = numbersConst.slice(0);
-        for(var i = 0; i < numbersLength; ++i) {
+        for (var i = 0; i < numbersLength; ++i) {
             var value = numbers[Math.floor(Math.random(numbers.length) * (numbersLength - i))];
             tds[i].innerHTML = numbersLength == value ? '' : value;
             numbers.splice(numbers.indexOf(value), 1);
             tds[i].addEventListener('click', logicFc, false);
         }    
-    }
+    },
+        buildFields = function() {
+            removeFields();
+            createFields();
+            fillFields();
+        }
+        
+    buildFields();
     
-    fillFields();
-    
-    document.getElementById('newgame').addEventListener('click', fillFields, false);
+    document.getElementById('newgame').addEventListener('click', buildFields, false);
 })()
